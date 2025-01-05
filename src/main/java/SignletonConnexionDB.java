97bd110a6507ee5771274c87bbc5302783daf892
saadkhadir <saad.khadir2-etu@etu.univh2c.ma>
2025-01-05 21:09:32 +0100

    commit

--- src/main/java/SignletonConnexionDB.java
+++ src/main/java/SignletonConnexionDB.java
@@ -1,4 +1,33 @@
-package PACKAGE_NAME;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
-public class SignletonConnexionDB {
-}
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+    public class SignletonConnexionDB {
+        private static Connection connection;
+
+        static {
+            try {
+                // Chargez le driver JDBC
+                Class.forName("com.mysql.cj.jdbc.Driver");
+                // Initialisez la connexion
+                connection = DriverManager.getConnection(
+                        "jdbc:mysql://localhost:3306/prep2", // Remplacez par votre URL JDBC
+                        "root", // Remplacez par votre utilisateur MySQL
+                        "Elgrandejcole03" // Remplacez par votre mot de passe MySQL
+                );
+            } catch (ClassNotFoundException | SQLException e) {
+                e.printStackTrace();
+                throw new RuntimeException("Erreur lors de la connexion à la base de données");
+            }
+        }
+
+        public static Connection getConnection() {
+            return connection;
+        }
+    }
+
+
