72d37809c06334d4cefa1eef977653111ea08532
saadkhadir <saad.khadir2-etu@etu.univh2c.ma>
2025-01-05 21:03:32 +0100

    commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,16 @@
+module com.example.exam {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+    requires com.almasb.fxgl.all;
+
+    opens com.example.exam to javafx.fxml;
+    exports com.example.exam;
+}
